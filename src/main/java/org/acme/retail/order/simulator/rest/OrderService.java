0d7b7448b9fbaf771f08b8b0507fece3984e30a5
Bernard Tison <bernard.tison@gmail.com>
2023-03-15 13:14:39 +0100

    changes for monolith version of the Globex store app

--- src/main/java/org/acme/retail/order/simulator/rest/OrderService.java
+++ src/main/java/org/acme/retail/order/simulator/rest/OrderService.java
@@ -10,7 +10,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/order")
+@Path("/services/order")
 @RegisterRestClient(configKey="order-service")
 public interface OrderService {
 
